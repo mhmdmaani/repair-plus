aa57521b81fe04a614bf28caacf18239a90c87f5
mhmdmaani <mohammedmaani@hotmail.com>
2024-06-12 11:50:17 +0200

    fix list

--- src/modules/home/SelectDevice.tsx
+++ src/modules/home/SelectDevice.tsx
@@ -3,6 +3,7 @@ import { useAllBrands } from '@/hooks/admin/useBrands';
 import { useDevice } from '@/hooks/admin/useDevices';
 import { useBrandTree } from '@/hooks/useBrandTree';
 import { useDeviceTree } from '@/hooks/useDeviceTree';
+import { useSettings } from '@/hooks/useSettings';
 import {
   Box,
   Card,
@@ -10,10 +11,11 @@ import {
   Grid,
   List,
   ListItem,
-  ListItemButton,
-  ListItemIcon,
   ListItemText,
+  ListItemIcon,
+  Typography,
   styled,
+  Fade,
 } from '@mui/material';
 import { Brand, Device, Repair } from 'prisma/prisma-client';
 import React from 'react';
@@ -26,10 +28,70 @@ const CustomListItem = styled(ListItem)<{
       ? `
     background-color: #f5f5f5;
     border-left: 4px solid #3f51b5;
+    padding-left: 16px;
+    margin-bottom: 10px;
+    height:80px;
+    gap: 10px;
+    transition:all 0.3s ease-in-out;
+    cursor: pointer;
   `
-      : ''};
+      : `
+    padding-left: 20px
+    margin-bottom: 10px;
+    height:80px;
+    gap: 10px;
+    transition:all 0.3s ease-in-out;
+    cursor: pointer;
+      `};
 `;
+
+const CustomText = styled('div')`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  flex: 1;
+`;
+
+const Image = styled('img')`
+  width: 50px;
+  height: auto;
+`;
+
+const Title = styled(Typography)`
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+  @media (max-width: 600px) {
+    font-size: 1.5rem;
+  }
+  @media (max-width: 400px) {
+    font-size: 1.2rem;
+  }
+`;
+
+const Tagline = styled(Typography)`
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+  @media (max-width: 600px) {
+    font-size: 1.5rem;
+  }
+  @media (max-width: 400px) {
+    font-size: 1rem;
+  }
+`;
+
+const Divider = styled('div')`
+  width: 120px;
+  height: 5px;
+  background: ${(props) => props.theme.palette.primary.main};
+  margin-top: 10px;
+  border-radius: 20px;
+  margin-bottom: 20px;
+`;
+
 export default function SelectDevice() {
+  const { data: settings } = useSettings();
   const { data: brands, isLoading } = useAllBrands();
   const [selectedBrand, setSelectedBrand] = React.useState<Brand | null>(null);
   const [selectedDevice, setSelectedDevice] = React.useState<Device | null>(
@@ -41,7 +103,23 @@ export default function SelectDevice() {
   const { data: brand } = useBrandTree(selectedBrand?.id || '');
   const { data: device } = useDeviceTree(selectedDevice?.id || '');
   return (
-    <Container>
+    <Container
+      style={{
+        marginTop: 20,
+        marginBottom: 20,
+      }}
+    >
+      <Fade in timeout={1500}>
+        <Tagline
+          data-aos={'fade'}
+          variant='h5'
+          color={'primary'}
+          fontWeight={'bold'}
+        >
+          About Us
+        </Tagline>
+      </Fade>
+      <Divider />
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6} md={4} lg={4}>
           <Card
@@ -62,16 +140,11 @@ export default function SelectDevice() {
                   selected={selectedBrand?.id === brand.id}
                 >
                   <ListItemIcon>
-                    <img
-                      src={brand?.logo || ''}
-                      alt={brand.name}
-                      width={30}
-                      height={30}
-                    />
+                    <Image src={brand?.logo || ''} alt={brand.name} />
                   </ListItemIcon>
-                  <ListItemButton>
+                  <CustomText>
                     <ListItemText primary={brand.name} />
-                  </ListItemButton>
+                  </CustomText>
                 </CustomListItem>
               ))}
             </List>
@@ -96,16 +169,16 @@ export default function SelectDevice() {
                   selected={selectedDevice?.id === device.id}
                 >
                   <ListItemIcon>
-                    <img
+                    <Image
                       src={device?.image || ''}
                       alt={brand.name}
                       width={30}
                       height={30}
                     />
                   </ListItemIcon>
-                  <ListItemButton>
+                  <CustomText>
                     <ListItemText primary={device.name} />
-                  </ListItemButton>
+                  </CustomText>
                 </CustomListItem>
               ))}
             </List>
@@ -130,16 +203,17 @@ export default function SelectDevice() {
                   selected={selectedRepair?.id === repair.id}
                 >
                   <ListItemIcon>
-                    <img
+                    <Image
                       src={repair?.image || ''}
                       alt={repair.name}
                       width={30}
                       height={30}
                     />
                   </ListItemIcon>
-                  <ListItemButton>
+                  <CustomText>
                     <ListItemText primary={repair.name} />
-                  </ListItemButton>
+                    <Typography variant='body2'>{`${repair.sellPrice}${settings?.currencySymbol}`}</Typography>
+                  </CustomText>
                 </CustomListItem>
               ))}
             </List>
