57da550fcebb84f5c21cff513a425fadf753aa5a
mhmdmaani <mohammedmaani@hotmail.com>
2024-06-28 16:37:15 +0200

    change api url

--- src/api/settings.ts
+++ src/api/settings.ts
@@ -6,7 +6,7 @@ export const STRIPE_PUBLISHABLE_KEY =
 export const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000/api'
-    : 'https://www.nationalquickcourier.co.uk/api';
+    : 'https://repair-plus.vercel.app/api';
 
 export class GetSettings {
   static async getSettings() {
