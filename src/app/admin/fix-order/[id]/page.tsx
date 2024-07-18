063b961beb256bce54594f83a7460ab2745ec0b6
mhmdmaani <mohammedmaani@hotmail.com>
2024-07-18 16:05:50 +0200

    fix order create

--- src/app/admin/fix-order/[id]/page.tsx
+++ src/app/admin/fix-order/[id]/page.tsx
@@ -1,5 +1,12 @@
+import FixOrderForm from '@/modules/admin/fix-order/FixOrderForm';
 import React from 'react';
 
-export default function EditFix() {
-  return <div>page</div>;
+export default function EditFix({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  return <FixOrderForm id={params.id} />;
 }
--- src/app/admin/fix-order/create/page.tsx
+++ src/app/admin/fix-order/create/page.tsx
@@ -2,5 +2,5 @@ import FixOrderForm from '@/modules/admin/fix-order/FixOrderForm';
 import React from 'react';
 
 export default function CreateFix() {
-  return <FixOrderForm />;
+  return <FixOrderForm id={undefined} />;
 }
