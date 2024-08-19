cc0babf51da03341ca99d32415ab827274b8ad43
mhmdmaani <mohammedmaani@hotmail.com>
2024-08-19 12:37:30 +0200

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,34 +1,35 @@
+import { FRONT_END_URL } from '@/api/settings';
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const links = [
     {
-      url: `${process.env.BASE_URL}`,
+      url: `${FRONT_END_URL}`,
       lastModified: new Date(),
     },
     {
-      url: `${process.env.BASE_URL}/about`,
+      url: `${FRONT_END_URL}/about`,
       lastModified: new Date(),
     },
     {
-      url: `${process.env.BASE_URL}/contact`,
+      url: `${FRONT_END_URL}/contact`,
       lastModified: new Date(),
     },
     {
-      url: `${process.env.BASE_URL}/fix/brands`,
+      url: `${FRONT_END_URL}/fix/brands`,
       lastModified: new Date(),
     },
 
     {
-      url: `${process.env.BASE_URL}/fix/categories`,
+      url: `${FRONT_END_URL}/fix/categories`,
       lastModified: new Date(),
     },
     {
-      url: `${process.env.BASE_URL}/terms`,
+      url: `${FRONT_END_URL}/terms`,
       lastModified: new Date(),
     },
     {
-      url: `${process.env.BASE_URL}/privacy`,
+      url: `${FRONT_END_URL}/privacy`,
       lastModified: new Date(),
     },
   ];
