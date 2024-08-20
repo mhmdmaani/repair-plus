27e248f361cbabaf63938ca2c172ef9c213d4d28
mhmdmaani <mohammedmaani@hotmail.com>
2024-08-20 09:43:01 +0200

    dynamic sitemaap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,80 @@
 import { FRONT_END_URL } from '@/api/settings';
 import { MetadataRoute } from 'next';
+import { PrismaClient } from 'prisma/prisma-client';
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const links = [
+// Function to fetch dynamic brand data
+async function getAllBrands() {
+  const prisma = new PrismaClient();
+  const brands = await prisma.brand.findMany({
+    where: {
+      isActive: true,
+    },
+    orderBy: {
+      order: 'desc',
+    },
+  });
+  await prisma.$disconnect();
+  return brands;
+}
+
+async function getAllCategories() {
+  const prisma = new PrismaClient();
+  const categories = await prisma.category.findMany({
+    where: {
+      isActive: true,
+    },
+    select: {
+      id: true,
+      updatedAt: true,
+    },
+    orderBy: {
+      order: 'desc',
+    },
+  });
+  await prisma.$disconnect();
+  return categories;
+}
+
+async function getAllModels() {
+  const prisma = new PrismaClient();
+  const models = await prisma.device.findMany({
+    where: {
+      isActive: true,
+    },
+    select: {
+      id: true,
+      updatedAt: true,
+    },
+    orderBy: {
+      order: 'desc',
+    },
+  });
+  await prisma.$disconnect();
+  return models;
+}
+
+async function getAllRepairs() {
+  const prisma = new PrismaClient();
+  const repairs = await prisma.repair.findMany({
+    where: {
+      isActive: true,
+    },
+    select: {
+      id: true,
+      updatedAt: true,
+    },
+    orderBy: {
+      order: 'desc',
+    },
+  });
+  await prisma.$disconnect();
+  return repairs;
+}
+
+// Function to generate the sitemap, combining static and dynamic entries
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  // Static links
+  const staticLinks = [
     {
       url: `${FRONT_END_URL}`,
       lastModified: new Date(),
@@ -19,7 +91,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${FRONT_END_URL}/fix/brands`,
       lastModified: new Date(),
     },
-
     {
       url: `${FRONT_END_URL}/fix/categories`,
       lastModified: new Date(),
@@ -34,5 +105,37 @@ export default function sitemap(): MetadataRoute.Sitemap {
     },
   ];
 
-  return links;
+  // Dynamic links from the database
+  const brands = await getAllBrands();
+  const brandsLink = brands.map((brand) => ({
+    url: `${FRONT_END_URL}/fix/brand/${brand.id}`,
+    lastModified: brand.updatedAt,
+  }));
+
+  const categories = await getAllCategories();
+  const categoriesLink = categories.map((category) => ({
+    url: `${FRONT_END_URL}/fix/category/${category.id}`,
+    lastModified: category.updatedAt,
+  }));
+
+  const models = await getAllModels();
+  const modelsLink = models.map((model) => ({
+    url: `${FRONT_END_URL}/fix/model/${model.id}`,
+    lastModified: model.updatedAt,
+  }));
+
+  const repairs = await getAllRepairs();
+  const repairsLink = repairs.map((repair) => ({
+    url: `${FRONT_END_URL}/fix/repair/${repair.id}`,
+    lastModified: repair.updatedAt,
+  }));
+
+  // Combine static and dynamic links
+  return [
+    ...staticLinks,
+    ...brandsLink,
+    ...categoriesLink,
+    ...modelsLink,
+    ...repairsLink,
+  ];
 }
